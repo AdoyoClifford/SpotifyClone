36e76cf22f0bcbf7aa0c3ac67edcbb6638486596
Adoyo Clifford <adoyoclifford@gmail.com>
2023-05-06 00:41:21 +0300

    initial commit

--- app/src/main/java/com/adoyo/auth/presentattion/LogIn.kt
+++ app/src/main/java/com/adoyo/auth/presentattion/LogIn.kt
@@ -12,18 +12,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,9 +66,35 @@ fun LogIn() {
                 )
             }
             Spacer(modifier = Modifier.height(32.dp))
-            OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(16.dp)
+                    ),
+                placeholder = {
+                    Text(
+                        text = "Email address or username",
+                        modifier = Modifier.padding(horizontal = 36.dp),
+                        style = TextStyle(fontWeight = FontWeight.Bold)
+                    )
+                })
             Spacer(modifier = Modifier.height(32.dp))
-            OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+            OutlinedTextField(value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(16.dp)
+                    ),
+                placeholder = {
+                    Text(
+                        text = "password",
+                        modifier = Modifier.padding(horizontal = 36.dp),
+                        style = TextStyle(fontWeight = FontWeight.Bold)
+                    )
+                })
 
             Spacer(modifier = Modifier.height(32.dp))
 
@@ -73,11 +104,42 @@ fun LogIn() {
                     style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold)
                 )
             }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(text = "Or continue with")
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.facebook),
+                    contentDescription = stringResource(
+                        id = R.string.facebook
+                    ),
+                    modifier = Modifier.size(50.dp)
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = stringResource(
+                        id = R.string.google
+                    ),
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Forgot your password")
+            }
         }
     }
 }
 
-@Preview
+@Preview(device = "id:pixel_7_pro", backgroundColor = 0xFFFFFFFF, showSystemUi = true)
 @Composable
 fun LonInPreview() {
     LogIn()
